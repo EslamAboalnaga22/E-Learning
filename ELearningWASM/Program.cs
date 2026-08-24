5909e2c6d1c25038b3f0de7db0072fd0abbba2a7
Eslam Aboalnaga <eslamnga220@gamil.com>
2026-08-24 19:11:18 +0300

    test branch commit

--- ELearningWASM/Program.cs
+++ ELearningWASM/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+// Git test
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
@@ -13,5 +14,5 @@ builder.Services.AddWsamDI(builder.Configuration);
 
 await builder.Build().RunAsync();
 
-////password --> P@ssword123
+//password --> P@ssword123
 
